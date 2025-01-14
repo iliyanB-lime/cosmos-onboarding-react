4882e78f5b0c7a2fc7a3469e619f950ff7de5f56
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2025-01-14 14:44:28 +0200

    Add: wrap application with chain provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { wallets } from "@cosmos-kit/keplr";
+import { ChainProvider } from "@cosmos-kit/react";
+import { assets, chains } from "chain-registry";
+
+// Import this in your top-level route/layout
+import "@interchain-ui/react/styles";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <ChainProvider chains={chains} assetLists={assets} wallets={wallets}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello World</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ChainProvider>
+  );
 }
 
-export default App
+export default App;
