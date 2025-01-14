7ad893dc6f3dff962839c20f51e178e74e45445f
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2025-01-14 16:29:29 +0200

    Add: extend useWalletConnection to return chain assets

--- src/hooks/useWalletConnection.ts
+++ src/hooks/useWalletConnection.ts
@@ -1,11 +1,19 @@
 import { useChain } from "@cosmos-kit/react";
-import { chains } from "chain-registry";
+import { chains, assets } from "chain-registry";
+import { AssetList } from "@chain-registry/types";
 import { DEFAULT_CHAIN_ID } from "../constants";
+import { useMemo } from "react";
 
 const chain = chains.find((c) => c.chain_id === DEFAULT_CHAIN_ID);
 
 export const useWalletConnection = () => {
   const chainData = useChain(chain?.chain_name || "", false);
+  const chainassets = useMemo(
+    () =>
+      (assets.find((c) => c.chain_name === chain?.chain_name) as AssetList) ||
+      [],
+    []
+  );
 
-  return { ...chainData };
+  return { ...chainData, assets: chainassets };
 };
