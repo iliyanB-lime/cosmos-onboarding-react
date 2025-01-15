819d0d274af81dd72f5d4fc7c59242729ec17e76
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2025-01-15 15:15:50 +0200

    Add: mantra LP contract address and more chain ids

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
-export const DEFAULT_CHAIN_ID = "osmo-test-5";
-export const DEFAULT_ASSET_DENOM = "uosmo";
+export const DEFAULT_CHAIN_ID = "mantra-dukong-1";
+// export const DEFAULT_CHAIN_ID = "celestia"; // Celestia Mainnet
+// export const DEFAULT_CHAIN_ID = "grand-1"; // Noble Testnet
+// export const DEFAULT_CHAIN_ID = 'osmosis-testnet' // Osmosis Testnet
+export const MANTRA_LIQUIDITY_POOLS_CONTRACT =
+  "mantra1us7rryvauhpe82fff0t6gjthdraqmtm5gw8c808f6eqzuxmulacqzkzdal";
